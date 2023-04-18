2e090683db8b176281fa5b761273450744c88673
ali taslimpoor <shayannniiii@gmail.com>
2023-04-18 17:08:56 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Unauthorized Access');
+      }
+    })
+    .then(data => {
+      const newId = data.id;
+      const idNode = document.createElement('p');
+      idNode.innerHTML = newId;
+      document.body.appendChild(idNode);
+    })
+    .catch(error => {
+      const errorNode = document.createElement('p');
+      errorNode.innerHTML = error.message;
+      document.body.appendChild(errorNode);
+    });
+  }
+  
